258315b2014ac2f6d54b58898b0b82cea06da5df
TanakaLun <115213949+TanakaLun@users.noreply.github.com>
2025-03-03 13:16:43 +0800

    Update mksu_susfs_vfs_fix.sh

--- mksu_susfs_vfs_fix.sh
+++ mksu_susfs_vfs_fix.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# 修复换行符问题
+sed -i 's/\r$//' mksu_susfs_vfs_fix.sh
+
+# 修改 ksud.c
 sed -i '/#include "selinux\/selinux.h"/a \
 bool ksu_vfs_read_hook = true;\n\
 bool ksu_execveat_hook = true;\n\
@@ -6,8 +12,8 @@ EXPORT_SYMBOL(ksu_vfs_read_hook);\n\
 EXPORT_SYMBOL(ksu_execveat_hook);\n\
 EXPORT_SYMBOL(ksu_input_hook);' ./kernel/ksud.c
 
+# 修改 ksud.h
 sed -i '/#include <linux\/types.h>/a \
 extern bool ksu_vfs_read_hook;\n\
 extern bool ksu_execveat_hook;\n\
 extern bool ksu_input_hook;' ./kernel/ksud.h
-
